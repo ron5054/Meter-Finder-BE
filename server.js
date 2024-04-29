f9021a770de613b885f16f6059dc0fd6a87c27ac
Yaron Levy <ron5054@gmail.com>
2024-04-29 16:38:42 +0300

    fix deployment

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
+app.get('/**', (req, res) => {
+  res.sendFile(path.resolve('public/index.html'))
+})
+
 app.post('/add', async (req, res) => {
   const newMeter = req.body
   try {
