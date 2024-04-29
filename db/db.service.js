eed3ab665bfd0e79e52d60c7fd05ef2bbd59c7a1
Yaron Levy <ron5054@gmail.com>
2024-04-29 16:08:18 +0300

    first commit

---
+++ db/db.service.js
@@ -0,0 +1,34 @@
+import mongoDB from 'mongodb'
+const { MongoClient } = mongoDB
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const dbService = {
+  getCollection,
+}
+
+var dbConn = null
+
+async function getCollection(collectionName) {
+  try {
+    const db = await connect()
+    const collection = await db.collection(collectionName)
+    return collection
+  } catch (err) {
+    throw err
+  }
+}
+
+async function connect() {
+  if (dbConn) return dbConn
+  try {
+    const client = await MongoClient.connect(process.env.DB_URL)
+
+    const db = client.db('Meter-Finder')
+    dbConn = db
+    return db
+  } catch (err) {
+    logger.error('Cannot Connect to DB', err)
+    throw err
+  }
+}
---
+++ server.js
@@ -0,0 +1,47 @@
+import express from 'express'
+import cors from 'cors'
+import { createServer } from 'http'
+import { dbService } from './db/db.service.js'
+
+const app = express()
+const server = createServer(app)
+app.use(express.json())
+
+const corsOptions = {
+  origin: [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+    'http://127.0.0.1:5173',
+    'http://localhost:5173',
+  ],
+  credentials: true,
+}
+
+app.use(cors(corsOptions))
+
+app.post('/add', async (req, res) => {
+  const newMeter = req.body
+  try {
+    const collection = await dbService.getCollection('meter')
+    const meter = await collection.findOne({ num: newMeter.num })
+    if (meter) throw new Error('Meter already exists')
+    else await collection.insertOne(newMeter)
+    res.json({ success: true })
+  } catch (err) {
+    res.json({ success: false, message: err.message })
+  }
+})
+
+app.get('/meter/:num', async (req, res) => {
+  try {
+    const collection = await dbService.getCollection('meter')
+    const meter = await collection.findOne({ num: req.params.num })
+    if (!meter) throw new Error('Meter not found')
+    res.json(meter)
+  } catch (err) {
+    res.json({ success: false, message: err.message })
+  }
+})
+
+const port = 3030
+server.listen(port, () => console.log(`Server is running on port ${port}`))
